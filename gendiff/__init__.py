adb77af317182d4ce9dbdf85c6f396853be47c59
Julia Didenko <iuliia.didenko@gmail.com>
2022-09-30 00:10:13 +0300

    Add facade to tools from generate_diff

---
+++ gendiff/__init__.py
@@ -0,0 +1,12 @@
+from gendiff.tools.generate_diff import generate_diff
+from gendiff.tools.generate_diff import get_dict_from_json
+from gendiff.tools.generate_diff import jsonify_repr
+from gendiff.tools.generate_diff import compare_data
+
+
+__all__ = (
+    'generate_diff',
+    'get_dict_from_json',
+    'jsonify_repr',
+    'compare_data',
+)
